60e8f5f74f8dd22f98bd83ce6096d13e92a189f9
Dank Rabbit <47937860+dankrabbit@users.noreply.github.com>
2024-10-01 13:44:05 -0700

    Moved speed cap to rotation, lowered click volume

--- joel.js
+++ joel.js
@@ -4,18 +4,23 @@ let spd = 0;
 let click = new Audio("pop.mp3");
 
 function spin() {
-	click.cloneNode(true).play();
-	spd = Math.min(spd + 1, 100);
+	let snd = click.cloneNode(true);
+	snd.volume = 0.42;
+	snd.play();
+
+	spd = spd + 1;
 };
 
-function round(i) {
-	return Math.round(i * Math.pow(10, 2)) / Math.pow(10, 2);
+function round(i,digits) {
+	return Math.round(i * Math.pow(10, digits)) / Math.pow(10, digits);
 }
 
 setInterval(() => {
-	ang += spd;
+	ang += Math.min(spd, 150);
 	document.querySelector(".joel").style.transform = `rotate(${ang}deg)`;
 
-	spd = Math.max(round(spd - 0.02), 0);
-	document.getElementById("speed").textContent = spd;
+	spd = Math.max(round(spd - 0.05, 2), 0);
+
+	let txt = Math.round(spd) == spd && spd + ".00" || round(spd, 1) == spd && spd + "0" || spd
+	document.getElementById("speed").textContent = txt;
 }, 16);
